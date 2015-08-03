6efdbf8d83f592ee0ef163ca81f0465f97b02e12
erhsh <erhsh_165@126.com>
2015-08-03 10:41:53 +0800

    first commit

---
+++ src/main/java/org/blackcrystalinfo/backstage/bo/AppVersion.java
@@ -0,0 +1,41 @@
+package org.blackcrystalinfo.backstage.bo;
+
+public class AppVersion {
+	private String id;
+	private String last;
+	private String avail;
+	private String url;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+	public String getAvail() {
+		return avail;
+	}
+
+	public void setAvail(String avail) {
+		this.avail = avail;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
